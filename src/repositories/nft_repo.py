64853b13878ae966a86444cc78a5abc2c6eb746d
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-15 16:35:09 +0200

    nft repo dos

--- src/repositories/nft_repo.py
+++ src/repositories/nft_repo.py
@@ -9,4 +9,22 @@ class NFTRepository:
         if not os.path.exists(self.ruta):
             with open(self.ruta, "w") as f:
                 json.dump([], f)
+    
+    def guardar_token(self, token_dict):
+        tokens = self.cargar_todos()
+        tokens.append(token_dict)
+        with open(self.ruta, "w") as f:
+            json.dump(tokens, f, indent=4)
 
+    def cargar_todos(self):
+        with open(self.ruta, "r") as f:
+            return json.load(f)
+
+    def transferir_token(self, token_id, nuevo_owner):
+        tokens = self.cargar_todos()
+        for token in tokens:
+            if token["token_id"] == token_id:
+                token["owner"] = nuevo_owner
+                break
+        with open(self.ruta, "w") as f:
+            json.dump(tokens, f, indent=4)
