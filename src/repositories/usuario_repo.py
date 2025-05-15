ae9289a38aba009a017ef07d4f343ffd02d0ee07
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-15 16:33:23 +0200

    usuario repo dos

--- src/repositories/usuario_repo.py
+++ src/repositories/usuario_repo.py
@@ -10,4 +10,19 @@ class UsuarioRepository:
             with open(self.ruta, "w") as f:
                 json.dump([], f)
 
-    
+    def guardar_usuario(self, usuario_dict):
+        usuarios = self.cargar_todos()
+        usuarios.append(usuario_dict)
+        with open(self.ruta, "w") as f:
+            json.dump(usuarios, f, indent=4)
+
+    def buscar_usuario(self, username):
+        usuarios = self.cargar_todos()
+        for u in usuarios:
+            if u["username"] == username:
+                return u
+        return None
+
+    def cargar_todos(self):
+        with open(self.ruta, "r") as f:
+            return json.load(f)
