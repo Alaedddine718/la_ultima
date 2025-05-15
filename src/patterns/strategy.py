4c9daa215482cb118ad9bbba43ed8138da1b8630
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-15 16:57:11 +0200

    strategy dos

--- src/patterns/strategy.py
+++ src/patterns/strategy.py
@@ -8,4 +8,10 @@ class EstrategiaAlfabetica(DesempateStrategy):
     def resolver(self, opciones_con_empate):
         return sorted(opciones_con_empate)[0]
 
+class EstrategiaAleatoria(DesempateStrategy):
+    def resolver(self, opciones_con_empate):
+        return random.choice(opciones_con_empate)
 
+class EstrategiaProrroga(DesempateStrategy):
+    def resolver(self, opciones_con_empate):
+        return "PRORROGA"
