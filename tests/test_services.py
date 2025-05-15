687b5b354842f7d7073d72dc1a1cf70b99173b68
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-15 19:49:14 +0200

    test services uno

--- tests/test_services.py
+++ tests/test_services.py
@@ -1,1 +1,15 @@
+import pytest
+from services.poll_service import PollService
+
+class FakeRepo:
+    def __init__(self):
+        self.data = []
+
+    def guardar_encuesta(self, encuesta_dict):
+        self.data.append(encuesta_dict)
+
+    def cargar_todas(self):
+        return self.data
+
+
 
