236d42dda65017bee3c2ca7e837bbe540208b7ad
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-15 16:54:55 +0200

    observer uno

---
+++ src/patterns/observer.py
@@ -0,0 +1,12 @@
+class Observable:
+    def __init__(self):
+        self.observadores = []
+
+    def agregar_observador(self, observador):
+        self.observadores.append(observador)
+
+    def notificar_observadores(self, evento):
+        for obs in self.observadores:
+            obs.update(evento)
+
+
