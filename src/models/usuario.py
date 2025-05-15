fd39cd924c73bf13aae33cf3b7b956656f23fd9a
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-15 16:20:09 +0200

    usuario dos

--- src/models/usuario.py
+++ src/models/usuario.py
@@ -3,5 +3,14 @@ class Usuario:
         self.username = username
         self.password_hash = password_hash  # contraseña cifrada
         self.tokens = []  # lista de tokens NFT que tiene este usuario
+    def agregar_token(self, token):
+        self.tokens.append(token)
 
+    def transferir_token(self, token_id, nuevo_owner):
+        for token in self.tokens:
+            if token.token_id == token_id:
+                token.owner = nuevo_owner
+                self.tokens.remove(token)
+                return token
+        raise Exception("Token no encontrado")
    
