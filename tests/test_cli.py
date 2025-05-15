0ede22f297732862626cfd24347f5e3f7e8cd5d7
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-15 19:54:02 +0200

    test cli dos

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -15,4 +15,19 @@ class DummyNFTRepo:
     def guardar_token(self, t): self.tokens.append(t)
     def cargar_todos(self): return self.tokens
 
+def test_cli_controller_crea_encuesta():
+    encuesta_repo = DummyRepo()
+    user_repo = DummyRepo()
+    nft_repo = DummyNFTRepo()
 
+    poll_service = PollService(encuesta_repo)
+    user_service = UserService(user_repo)
+    nft_service = NFTService(nft_repo)
+
+    cli = CLIController(poll_service, user_service, nft_service)
+
+    cli.ejecutar_comando("crear_encuesta ¿Te gusta Python? Sí,No 30")
+
+    encuestas = encuesta_repo.cargar_todas()
+    assert len(encuestas) == 1
+    assert encuestas[0]["pregunta"] == "¿Te gusta Python?"
