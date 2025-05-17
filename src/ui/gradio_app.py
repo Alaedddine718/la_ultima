a5981098d75bc3cf38a4f232f0d2ef3ef7067302
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-17 18:03:00 +0200

    last changes

--- src/ui/gradio_app.py
+++ src/ui/gradio_app.py
@@ -1,22 +1,14 @@
-# src/ui/gradio_app.py
-import sys
-import os
-sys.path.insert(0, os.path.abspath("src"))
+import gradio as gr
+from controllers.ui_controller import UIController
 from services.poll_service import PollService
 from services.user_service import UserService
 from services.nft_service import NFTService
 from services.chatbot_service import ChatbotService
-
 from repositories.encuesta_repo import EncuestaRepository
 from repositories.usuario_repo import UsuarioRepository
 from repositories.nft_repo import NFTRepository
 
-from controllers.ui_controller import UIController
-from config import cargar_config
-
 def lanzar_ui():
-    config = cargar_config()
-
     encuesta_repo = EncuestaRepository()
     usuario_repo = UsuarioRepository()
     nft_repo = NFTRepository()
@@ -27,13 +19,17 @@ def lanzar_ui():
     chatbot_service = ChatbotService(poll_service)
 
     ui_controller = UIController(poll_service, user_service, nft_service, chatbot_service)
-    ui_controller.lanzar()
 
-# main.py (fuera de src)
-# Asegúrate de que main.py tenga esto:
-# from ui.gradio_app import lanzar_ui
-# if __name__ == "__main__":
-#     lanzar_ui()
+    interfaz = gr.Interface(
+        fn=lambda mensaje, usuario: ui_controller.responder_chat(usuario, mensaje),
+        inputs=["text", "text"],
+        outputs="text",
+        title="Plataforma de Votaciones para Streamers",
+        description="Escribe un mensaje y tu nombre de usuario"
+    )
+
+    interfaz.launch()
+
 
 
 
