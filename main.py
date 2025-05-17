d60ec341cefe16911929d9dde565e8e22e56e405
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-17 17:26:16 +0200

    m

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from src.ui.gradio_app import lanzar_ui
+import sys
+import os
+sys.path.insert(0, os.path.abspath("src"))
+
+from ui.gradio_app import lanzar_ui
 
 if __name__ == "__main__":
     lanzar_ui()
+
 
 
