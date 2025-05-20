c66c252842f8af218fd544fc022baae443086726
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-20 16:29:31 +0200

    cambios de repositories

--- src/repositories/encuesta_repo.py
+++ src/repositories/encuesta_repo.py
@@ -1,21 +1,11 @@
-import json
-import os
+from abc import ABC, abstractmethod
 
-class EncuestaRepository:
-    def __init__(self, ruta_archivo="data/encuestas.json"):
-        self.ruta = ruta_archivo
-        if not os.path.exists("data"):
-            os.makedirs("data")
-        if not os.path.exists(self.ruta):
-            with open(self.ruta, "w") as f:
-                json.dump([], f)
-
+class EncuestaRepository(ABC):
+    @abstractmethod
     def guardar_encuesta(self, encuesta_dict):
-        encuestas = self.cargar_todas()
-        encuestas.append(encuesta_dict)
-        with open(self.ruta, "w") as f:
-            json.dump(encuestas, f, indent=4)
+        pass
 
-    def cargar_todas(self):
-        with open(self.ruta, "r") as f:
-            return json.load(f)
+    @abstractmethod
+    def obtener_encuesta(self, encuesta_id):
+        pass
+
