776b763b8d2717c43e24f86680d84ade74f183e6
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-20 18:26:22 +0200

    correccion del error de nft_repo

--- src/repositories/nft_repo.py
+++ src/repositories/nft_repo.py
@@ -1,4 +1,18 @@
+from abc import ABC, abstractmethod
 from src.config import cargar_config
+
+class NFTRepository(ABC):
+    @abstractmethod
+    def guardar_token(self, token_dict):
+        pass
+
+    @abstractmethod
+    def obtener_tokens_usuario(self, username):
+        pass
+
+    @abstractmethod
+    def transferir_token(self, token_id, nuevo_owner):
+        pass
 
 def crear_nft_repo():
     config = cargar_config()
@@ -9,11 +23,11 @@ def crear_nft_repo():
         return NFTRepositoryMongo(config["mongo_uri"])
 
     elif tipo == "firebase":
-        from src.repositories.firebase.nft_repo import NFTRepositoryFirebase
+        from src.repositories.firebase.nft_repo_firebase import NFTRepositoryFirebase
         return NFTRepositoryFirebase(config["firebase_key"])
 
     elif tipo == "neo4j":
-        from src.repositories.neo4j.nft_repo import NFTRepositoryNeo4j
+        from src.repositories.neo4j.nft_repo_neo4j import NFTRepositoryNeo4j
         return NFTRepositoryNeo4j(
             config["neo4j_uri"],
             config["neo4j_user"],
@@ -22,4 +36,5 @@ def crear_nft_repo():
 
     else:
         raise Exception("Tipo de base de datos no soportado: " + tipo)
+
 
