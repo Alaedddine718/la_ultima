f18b10b5bc5ff66ecd4bfa1f0956117d290fc2c4
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-20 19:01:28 +0200

    usuario_repo

--- src/repositories/usuario_repo.py
+++ src/repositories/usuario_repo.py
@@ -1,35 +1,36 @@
-# src/repositories/encuesta_repo.py
 from abc import ABC, abstractmethod
 from src.config import cargar_config
 
-class EncuestaRepository(ABC):
+class UsuarioRepository(ABC):
     @abstractmethod
-    def guardar_encuesta(self, encuesta_dict):
+    def guardar_usuario(self, usuario_dict):
         pass
 
     @abstractmethod
-    def obtener_encuesta(self, encuesta_id):
+    def buscar_usuario(self, username):
         pass
 
-def crear_encuesta_repo():
+def crear_usuario_repo():
     config = cargar_config()
     tipo = config.get("base_datos")
 
     if tipo == "mongo":
-        from src.repositories.mongo.encuesta_repo_mongo import EncuestaRepositoryMongo
-        return EncuestaRepositoryMongo(config["mongo_uri"])
+        from src.repositories.mongo.usuario_repo_mongo import UsuarioRepositoryMongo
+        return UsuarioRepositoryMongo(config["mongo_uri"])
 
     elif tipo == "firebase":
-        from src.repositories.firebase.encuesta_repo_firebase import EncuestaRepositoryFirebase
-        return EncuestaRepositoryFirebase(config["firebase_key"])
+        from src.repositories.firebase.usuario_repo_firebase import UsuarioRepositoryFirebase
+        return UsuarioRepositoryFirebase(config["firebase_key"])
 
     elif tipo == "neo4j":
-        from src.repositories.neo4j.encuesta_repo_neo4j import EncuestaRepositoryNeo4j
-        return EncuestaRepositoryNeo4j(
+        from src.repositories.neo4j.usuario_repo_neo4j import UsuarioRepositoryNeo4j
+        return UsuarioRepositoryNeo4j(
             config["neo4j_uri"],
             config["neo4j_user"],
             config["neo4j_password"]
         )
+
     else:
         raise Exception("Tipo de base de datos no soportado: " + tipo)
+
 
