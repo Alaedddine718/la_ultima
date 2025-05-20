5eb51a22a6e25c444c6a8d28e42e2cd66e5235ef
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-20 16:34:32 +0200

    cambios

--- src/repositories/usuario_repo.py
+++ src/repositories/usuario_repo.py
@@ -8,4 +8,26 @@ class UsuarioRepository(ABC):
     @abstractmethod
     def buscar_usuario(self, username):
         pass
+from src.config import cargar_config
+
+def crear_usuario_repo():
+    config = cargar_config()
+    tipo = config.get("base_datos")
+
+    if tipo == "mongo":
+        from src.repositories.mongo.usuario_repo_mongo import UsuarioRepositoryMongo
+        return UsuarioRepositoryMongo(config["mongo_uri"])
+    
+    elif tipo == "firebase":
+        from src.repositories.firebase.usuario_repo_firebase import UsuarioRepositoryFirebase
+        return UsuarioRepositoryFirebase(config["firebase_key"])
+    
+    elif tipo == "neo4j":
+        from src.repositories.neo4j.usuario_repo_neo4j import UsuarioRepositoryNeo4j
+        return UsuarioRepositoryNeo4j(
+            config["neo4j_uri"], config["neo4j_user"], config["neo4j_password"]
+        )
+    
+    else:
+        raise Exception("Tipo de base
 
