25fd1e1ce4a7a2410afc9056d63ef84cf8ab72d0
Alaedddine718 <adaoudi@student.universidadunie.com>
2025-05-20 17:29:28 +0200

    modification

--- src/repositories/encuesta_repo.py
+++ src/repositories/encuesta_repo.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from src.config import cargar_config
 
 class EncuestaRepository(ABC):
     @abstractmethod
@@ -8,4 +9,28 @@ class EncuestaRepository(ABC):
     @abstractmethod
     def obtener_encuesta(self, encuesta_id):
         pass
+
+def crear_encuesta_repo():
+    config = cargar_config()
+    tipo = config.get("base_datos")
+
+    if tipo == "mongo":
+        from src.repositories.mongo.encuesta_repo_mongo import EncuestaRepositoryMongo
+        return EncuestaRepositoryMongo(config["mongo_uri"])
+
+    elif tipo == "firebase":
+        from src.repositories.firebase.encuesta_repo_firebase import EncuestaRepositoryFirebase
+        return EncuestaRepositoryFirebase(config["firebase_key"])
+
+    elif tipo == "neo4j":
+        from src.repositories.neo4j.encuesta_repo_neo4j import EncuestaRepositoryNeo4j
+        return EncuestaRepositoryNeo4j(
+            config["neo4j_uri"],
+            config["neo4j_user"],
+            config["neo4j_password"]
+        )
+
+    else:
+        raise Exception("Tipo de base de datos no soportado: " + tipo)
+
 
--- src/repositories/nft_repo.py
+++ src/repositories/nft_repo.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from src.config import cargar_config
 
 class NFTRepository(ABC):
     @abstractmethod
@@ -12,4 +13,28 @@ class NFTRepository(ABC):
     @abstractmethod
     def transferir_token(self, token_id, nuevo_owner):
         pass
+
+def crear_nft_repo():
+    config = cargar_config()
+    tipo = config.get("base_datos")
+
+    if tipo == "mongo":
+        from src.repositories.mongo.nft_repo_mongo import NFTRepositoryMongo
+        return NFTRepositoryMongo(config["mongo_uri"])
+
+    elif tipo == "firebase":
+        from src.repositories.firebase.nft_repo_firebase import NFTRepositoryFirebase
+        return NFTRepositoryFirebase(config["firebase_key"])
+
+    elif tipo == "neo4j":
+        from src.repositories.neo4j.nft_repo_neo4j import NFTRepositoryNeo4j
+        return NFTRepositoryNeo4j(
+            config["neo4j_uri"],
+            config["neo4j_user"],
+            config["neo4j_password"]
+        )
+
+    else:
+        raise Exception("Tipo de base de datos no soportado: " + tipo)
+
 
